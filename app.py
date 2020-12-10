1dcc3e3362acba45fa0b1c7769572a0e24f70784
garudaplus <kishorerbjv@gmail.com>
2020-12-10 22:44:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def hello_world():
-    output="LATEST VERSION OF CODE : V2"
+    output="Hello world from method: "+request.method
     return output
